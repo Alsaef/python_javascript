c9e9d2d5d74870952443de859de164f7bd4f9da8
ASR <saef.ratul@gmail.com>
2024-08-22 00:20:35 +0600

    add file

---
+++ javascriptArray.js
@@ -0,0 +1,10 @@
+const list=["apple", "banana", "cherry", "apple", "cherry"]
+
+
+
+
+// add list of array
+list.push('mango')
+console.log(list)
+// access index
+console.log('index:', list[1])
---
+++ python_list.py
@@ -0,0 +1,13 @@
+list=["apple", "banana", "cherry", "apple", "cherry"]
+
+
+
+
+# add list of list 
+
+list.append('mango')
+
+print(list)
+
+# access index
+print("index:", list[1])
