daa3515c5046764f67243e5e60eb381432e2a25d
ASR <saef.ratul@gmail.com>
2024-08-22 19:02:28 +0600

    folder and  file  change

---
+++ object and dictionary/dictionary.py
@@ -0,0 +1,9 @@
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+# access Item
+print("Access Item: ",thisdict['brand'])
---
+++ object and dictionary/object.js
@@ -0,0 +1,14 @@
+const thisdict =	{
+    brand: "Ford",
+    model: "Mustang",
+    year: 1964
+  }
+  console.log(thisdict);
+
+  // access item
+
+  console.log('Access Item: ',thisdict.brand);
+
+  // change Item
+
+  
