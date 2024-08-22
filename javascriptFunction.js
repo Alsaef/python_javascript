b66a7e43c601cdb1446c112502ac598084fc115c
ASR <saef.ratul@gmail.com>
2024-08-22 16:21:41 +0600

    function

---
+++ javascriptFunction.js
@@ -0,0 +1,10 @@
+function myName(){
+    console.log('ratul');
+}
+myName()
+
+function Adition(a,b) {
+    console.log("Adition Result: ",a+b);
+}
+
+Adition(10,20)
---
+++ python_function.py
@@ -0,0 +1,10 @@
+def myName():
+    print('ratul')
+    
+myName()
+
+
+def Adition(a,b):
+    print("addition Result:", a+b)
+    
+Adition(10,20)
